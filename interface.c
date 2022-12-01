e1c1bfeba0c68f20a60c07643f28c4e8d79b030f
Temple Tran <tkt5248@psu.edu>
2022-11-30 22:29:32 -0500

    More signal handler stuff

--- interface.c
+++ interface.c
@@ -9,17 +9,17 @@ void mm_init(enum policy_type policy, void *vm, int vm_size, int num_frames, int
     // Make the virtual memory space inaccessible
     if (mprotect(vm, vm_size, PROT_NONE) == -1)
     {
-        perror("mprotect() error - mm_init()");
+        perror("mprotect() error in mm_init()\n");
         return;
     }
 
-    memset(&sa, '\0', sizeof(sa));
+    sigemptyset(&sa.sa_mask);
     sa.sa_sigaction = &handler;
     sa.sa_flags = SA_SIGINFO;
     // register handler for SIGSEGV
     if (sigaction(SIGSEGV, &sa, NULL) < 0)
     {
-        perror("sigaction error - mm_init()");
+        perror("sigaction() error in mm_init()\n");
         return;
     } 
 
@@ -28,18 +28,6 @@ void mm_init(enum policy_type policy, void *vm, int vm_size, int num_frames, int
     will generate a SIGSEGV fault, and write a signal handler to catch this fault. Additionally,
     investigate how to determine if the rased fault is from a read/write to a page. Ensure
     that you understand how to write signal handers and leverage mprotect() before proceeding.*/
-
-    /*void handler(int sig)
-{
-    write(1, "success", strlen("success")); // printf is not recommended here, but should work as well
-    exit(0);
-}
-
-int main()
-{
-    struct sigaction sa;
-    int *a; // a will contain some garbage value
-    int b = *a; // trigger segmentation fault; transfer control to handler*/
 
 }
 
--- vmm.c
+++ vmm.c
@@ -5,6 +5,27 @@
 
 void handler(int sig, siginfo_t* info, void* ucontext)
 {
+    printf("transfer control to handler\n");
+    if (sig == SIGSEGV)
+    {
+        ucontext_t* ctx = (ucontext_t*)ucontext;
+        long long err = ctx->uc_mcontext.gregs[REG_ERR];
+        void* addr = info->si_addr;
+
+        if (err == 4)
+        {
+            printf("its a sigsegv from read access at address %p\n", addr);
+        }
+        else if (err == 6)
+        {
+            printf("its a sigsegv from write access at address %p\n", addr);
+        }
+        else
+        {
+            printf("its sigsegv from an unknown access with err = %lld at address %p\n", err, addr);
+        }
+    }
+    raise(SIGKILL);
     return;
 }
 
